f26b85ede72afdc4da7ef6180bc15ca5d5a42d8d
josep <josepborras8@gmail.com>
2016-04-05 16:33:40 +0200

    main loop

--- ambrosio/ambrosio.py
+++ ambrosio/ambrosio.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+class Ambrosio(object):
+
+    """Class for Ambrosio Personal Digital Butler
+
+
+    will run or hous"""
+    def __init__(self):
+        super(Ambrosio, self).__init__()
+
+    def mainloop(self):
+        # while True:
+        # command = get_command
+        # do_command(command)
+        # update
+        pass
+
+
 if __name__ = "__main__";
     print ""
---
+++ ambrosio/commandlist.py
@@ -0,0 +1,16 @@
+
+
+class commandlist(object):
+    
+    def __init__(self):
+        super(commandlist, self).__init__()
+        self.commands = []
+
+    def next(self):
+        pass
+
+    def sppend(self, command):
+        pass
+
+    def length(self):
+        pass
